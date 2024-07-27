c75d5f452e800cedbfc820627f1f36894c9c32e7
Eunbee Cho <123444582+Eunvivi@users.noreply.github.com>
2024-07-27 20:41:19 +0900

    Delete SQL_TIL directory

--- SQL_TIL/LeetCode/0570-managers-with-at-least-5-direct-reports/0570-managers-with-at-least-5-direct-reports.sql
+++
@@ -1,7 +0,0 @@
-SELECT m.name
-FROM Employee e
-INNER JOIN Employee m ON e.managerId = m.id
-GROUP BY e.managerId
-HAVING COUNT(*)>=5
-
-
--- SQL_TIL/LeetCode/0577-employee-bonus/0577-employee-bonus.sql
+++
@@ -1,5 +0,0 @@
-SELECT e.name AS name, b.bonus AS bonus
-FROM Employee e
-LEFT JOIN Bonus b
-ON e.empId = b.empId
-WHERE b.bonus IS NULL OR b.bonus < 1000
